6cfb37479039c960580c12f2fbd2daf7474c170a
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-18 22:03:02 +0530

    Create stringFunctions.py

---
+++ stringFunctions.py
@@ -0,0 +1,144 @@
+s = "this is a big string of stars"
+
+print(s.endswith("i",0,6))
+
+# r= s.center(40,"-")
+# print(r)
+
+
+
+#
+
+
+# s = "hello iam {}, {} is a very sweet name, my mother calls me {}"
+
+#  capitalize,casefold lower upper, index, count,len , type
+
+# name = "diksha"
+# r = s.format(name, name, name)
+# name = "tina"
+# t = s.format(name, name, name)
+# print(r,"\n",t)
+
+# r = s.split(" ",3)
+# print(r)
+
+
+# ans = s.capitalize()
+# ans = s.casefold()
+# r = s.lower()
+# print(ans)
+# print(r)
+# print(len(s))
+# t = "hello there"
+# r = s.upper()
+# r = s.lower()
+
+# r = s.count("this",5,19)
+# r = s.index("this", 8 , 12)
+
+# print(r)
+# //print(s)
+
+
+
+
+# ["","","tree","apple","my","","name"]
+
+# [8,9,6,5]=>[64,81,36,25]
+
+# def square(list):
+#     sq=[]
+#     for i in range (0,len(list)):
+#         sq.append(list[i]*list[i])
+#     return sq;
+#
+# print(square([1,2,5,7,9]))
+
+# str = "it is new day"
+#
+# list= str.split(" ")
+# list.reverse()
+# str=""
+# for i in range (0, len(list)):
+#     str = str+list[i]+" "
+# print(str)
+# "day new is it"
+# print(str.split(" "))
+
+# list= [1,2,3,4]
+#
+# list.reverse();
+# print(list)
+
+# print(len(list))
+
+# sum of elements of list =>
+#
+#
+# def sumList(list):
+#     return sum(list)
+#
+# print(sumList([9,8,5,7]))
+#
+# print(sumList([9,9,7,0,8,5,7]))
+# odd , even
+#take a list  and then count the even and odd numbers and print the count
+
+# list = [1,43,12,546,2,3,78,35,467,7,54]
+#
+# max = list[0]
+#
+# for ele in list :
+#     if(ele>max) :
+#         max=ele
+#
+# print("maximum number = ", max)
+
+# Q2 : find the max number from the list
+
+
+# evenNumberCount =0
+# oddNumberCount =0
+# for ele in list :
+#     if ele%2==0 :
+#         evenNumberCount = evenNumberCount +1
+#     else :
+#         oddNumberCount += 1
+#
+# print("even = ", evenNumberCount)
+# print("odd = ",oddNumberCount)
+
+
+# evedncount = 3
+#oddcount =5
+
+# sum = list[0] +list[1]+list[2]+list[3]+list[4]
+
+# sum =0
+#
+# for e in list :
+#     sum=sum+e
+
+# //print("total sum ", sum)
+
+
+# for ele in list :
+#     print("========",ele,"=========")
+
+# print("list")
+# list = [10,20,30,40,50]
+# # list.remove(30)
+# list.pop(4)
+# print(list)
+
+# print(list)
+# list.insert(-2, -10)
+# print(list)
+
+# list = [45]*10
+# print(list)
+
+
+
+
