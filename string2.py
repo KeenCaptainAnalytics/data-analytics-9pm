206fd2bcc15096822e4dede48b53fb69a2e42131
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-19 22:11:57 +0530

    Create string2.py

---
+++ string2.py
@@ -0,0 +1,43 @@
+
+s= "123456789"
+# slicing
+print(s[0:4])
+print(s[:4])
+print(s[:])
+print(s[3:7:3])
+print(s[::-2])
+
+
+# s = hex(64564)
+# s = str(s)
+# print(s.isdecimal())
+# print(s.isnumeric())
+# print(s.isdigit())
+
+
+
+# s = "                      y "
+#
+# print(s.isspace())
+
+
+# s= "the world is here567+"
+#
+#
+# print(s.islower())
+
+# s = "THE WORLD"
+#
+# print(s.isupper())
+
+# print(s.isdigit())
+
+
+
+# print(s.isalnum())
+
+# print(s.isalpha())
+
+# print(s.endswith("f", 5, 9))
+
+# print(s.startswith("th", 5, 9))
