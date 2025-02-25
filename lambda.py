d1b68ffdaf4e5a27f54db00a7422f02a351dc99a
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-25 21:59:52 +0530

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,62 @@
+# function => obj function arg , return , function called, variab;es
+
+def xyz(arg):
+    return arg*2
+
+
+def printIt(fun):
+    result = fun(123)
+    return result
+
+
+
+print(printIt(xyz))
+
+
+
+# obj = (print("a"), print('b'), print('c'), 2)
+# print(obj)
+# hello = lambda a,b,c : (print(a), print(a+b), print(a+b+c), 67+89)[3]
+#
+# #
+# print(hello("a", "b", "c"))
+# print(hello("1", "2", "3"))
+# a
+# # a + b
+# # a+b+c
+# addIt = lambda a, b: addNow
+# ProIt = lambda a, b: a*b
+# def addNow(a,b):
+#     return a+b
+
+# def doSomething(a,b,c, func):
+#     a = a +"Happy"
+#     b = b +"world"
+#     c = c+" birthday"
+#     b = c+b
+#     print(func(a,b))
+#
+# doSomething("1", "3", "87", addNow)
+
+
+
+# result = printIt()
+# print (result)
+# a = 78
+# # lambda function
+# num = lambda a,b, c : a+b
+#
+# print(num(56,89))
+
+
+# def num1(a, b):
+#     return a+b
+
+# def abc():
+#     print("hello world")
+#
+#
+# abc()
+#
+# abc()
+# abc()
