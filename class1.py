4bc386e4b1c48b6ae946716a2e7d602dd6d85ed4
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-26 21:57:16 +0530

    Update class1.py

--- class1.py
+++ class1.py
@@ -1,21 +1,47 @@
 # class
 
-class Car :
-    def __init__(self, para_brand, para_model, para_color,para_price ) :
-        self.brand = para_brand
-        self.model = para_model
-        self.color = para_color
-        self.price = para_price
+class Transport:
+    def __init__(self, name):
+        self.distance = 0
+        self.name= name
+    def printInfo(self):
+        print(self.distance, self.name)
+    def setName(self, name):
+        self.name = name
+    def setDistance(self, distance):
+        self.distance = distance
 
-    def print_info(self):
-        print(self.price, self.color, self.model, self.brand)
+#         is - a
+class Car(Transport):
+    def run(self):
+        print("the car is running")
 
 
-car1obj = Car("tata", "1234","black", 200000 )
-car2obj = Car("Maruti", "13434","white", 500000 )
+carobj1 = Car("car");
+carobj1.run()
+carobj1.setName("car")
+carobj1.setDistance(100)
+carobj1.printInfo();
 
-car1obj.print_info()
-car2obj.print_info()
+
+
+
+# class Car :
+#     def __init__(self, para_brand, para_model, para_color,para_price ) :
+#         self.brand = para_brand
+#         self.model = para_model
+#         self.color = para_color
+#         self.price = para_price
+#
+#     def print_info(self):
+#         print(self.price, self.color, self.model, self.brand)
+#
+#
+# car1obj = Car("tata", "1234","black", 200000 )
+# car2obj = Car("Maruti", "13434","white", 500000 )
+#
+# car1obj.print_info()
+# car2obj.print_info()
 
 # print(car1obj.model,car1obj.brand, car1obj.color, car1obj.price)
 # print(car2obj.model,car2obj.brand, car2obj.color, car2obj.price)
