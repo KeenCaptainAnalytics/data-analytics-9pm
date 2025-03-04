933dab3f5e4f07c5b11277067cace53cf0d20070
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-04 22:01:40 +0530

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+# 2 D Array
+
+arr = np.array([[1,2,3],[2,3,4]])
+print(arr
+      )
+# print(arr.dtype)
+
+
+
+
+
+# 1D Array
+# arr = np.array([1,2,3,4,5]);
+
+# arr  =  np.array([1,2,3,4,5,6,7,2]) //1D
+                  # 0 1 2 3
+
+# print(arr[3])
+
+
+# arr2d = np.array([])
+
+
+# 64, 64, 64, 64
+#  obj1
+#  ob2                ob3
+
+
+# print(arr.dtype)
+# print(type(arr))
+
+
+
+
+# list = [1,2,3,4]
+# numpy  = > external library
+# arrays = > similar type store  = > continous array / cpp , python
+
+# numpy = > numerical python
+# pip = > python package manager
+# packages  = > libraries
+#  collection  = > classes =  object => function propert
