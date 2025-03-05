dd54134f9a38b54a980ce069e687b6da2d9b66dd
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-05 21:21:45 +0530

    Create numpy2.py

---
+++ numpy2.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.array([1,20,31,128,255],dtype=np.uint8)
+
+print(arr)
+print(arr.dtype)
+
+
+# int8  = > 127 <-> -128
+# unsigned int 8  => 0-255
+# 8 = > 2^8
+# 64 = 8bytes ,    ram  = > 4GB => 4*1024*1024*1024B
+#
+#  2.1 km => 219378675534234567
+
