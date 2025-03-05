7978849a60dcf59372ce97bc1858b97f2c9d98ac
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-05 21:57:52 +0530

    Update numpy2.py

--- numpy2.py
+++ numpy2.py
@@ -1,9 +1,30 @@
 import numpy as np
 
-arr = np.array([1,20,31,128,255],dtype=np.uint8)
+csvdata = np.loadtxt('abc.csv',delimiter=',', dtype=str, skiprows=1)
 
-print(arr)
-print(arr.dtype)
+print(csvdata[0:5:])
+
+
+
+# transpose =>matrix 2d row ==columns
+
+# arr = np.array([[4,6,8],[6,8,9],[2,3,4],[5,7,9]])
+# print(arr)
+# print(arr.T)
+
+# arr= np.array([[[11,98],[4,8]],[[6,0],[3,8]]])
+# print(arr.ndim)
+# print(arr.itemsize)
+# print(arr.shape)
+
+
+# arrF = np.array([1.1,6.78,9.768],dtype=np.float16)
+# print(arrF.dtype)
+
+# arr = np.array([1,20,31,128,255],dtype=np.uint8)
+#
+# print(arr)
+# print(arr.dtype)
 
 
 # int8  = > 127 <-> -128
